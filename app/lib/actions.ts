06c858fe10000d4b040513a0a69989787304fa6d
Alvaro Medina <alvaro.mediina2003@gmail.com>
2025-07-11 11:18:08 -0300

    feat: :sparkles: Capitulo 13 completo

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -24,12 +24,14 @@ export async function createInvoices(formData: FormData) {
   });
   const amountInCents = amount * 100;
   const date = new Date().toISOString().split('T')[0];
-
-  await sql`
-    INSERT INTO invoices (customer_id, amount, status, date)
-    VALUES (${customerId}, ${amount}, ${status}, ${date})  
-  `;
-
+  try {
+    await sql`
+      INSERT INTO invoices (customer_id, amount, status, date)
+      VALUES (${customerId}, ${amount}, ${status}, ${date})  
+    `;
+  } catch (error) {
+    console.log(error);
+  }
   revalidatePath('/dashboard/invoices');
   redirect('/dashboard/invoices');
 }
@@ -45,12 +47,15 @@ export async function updateInvoice(id: string, formData: FormData) {
 
   const amountInCents = amount * 100;
 
-  await sql`
-    UPDATE invoices
-    SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
-    WHERE id = ${id}
-  `;
-
+  try {
+    await sql`
+      UPDATE invoices
+      SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+      WHERE id = ${id}
+    `;
+  } catch (error) {
+    console.log(error);
+  }
   revalidatePath('/dashboard/invoices');
   redirect('/dashboard/invoices');
 }
