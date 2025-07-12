6033e306629812499a93e626b3465102fe67cf18
Alvaro Medina <alvaro.mediina2003@gmail.com>
2025-07-12 10:30:36 -0300

    feat: :sparkles: Capítulo 16 terminado

--- app/dashboard/invoices/create/page.tsx
+++ app/dashboard/invoices/create/page.tsx
@@ -1,9 +1,14 @@
-import Form from '@/app/ui/invoices/create-form'
-import Breadcrumbs from '@/app/ui/invoices/breadcrumbs'
-import { fetchCustomers } from '@/app/lib/data'
+import Form from '@/app/ui/invoices/create-form';
+import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
+import { fetchCustomers } from '@/app/lib/data';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Create Customer',
+};
 
 export default async function Page() {
-  const customers = await fetchCustomers()
+  const customers = await fetchCustomers();
 
   return (
     <main>
@@ -19,5 +24,5 @@ export default async function Page() {
       />
       <Form customers={customers} />
     </main>
-  )
+  );
 }
--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -6,20 +6,24 @@ import { lusitana } from '@/app/ui/fonts';
 import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
 import { Suspense } from 'react';
 import { fetchInvoicesPages } from '@/app/lib/data';
+import { Metadata } from 'next';
 
+export const metadata: Metadata = {
+  title: 'Invoices',
+};
 
 export default async function Page(props: {
-        searchParams?: Promise<{
-        query?: string;
-        page?: string;
-    }>;
+  searchParams?: Promise<{
+    query?: string;
+    page?: string;
+  }>;
 }) {
-    const searchParams = await props.searchParams;
-    const query = searchParams?.query || '';
-    const currentPage = Number(searchParams?.page) || 1;
-    const totalPages = await fetchInvoicesPages(query);
+  const searchParams = await props.searchParams;
+  const query = searchParams?.query || '';
+  const currentPage = Number(searchParams?.page) || 1;
+  const totalPages = await fetchInvoicesPages(query);
 
-    return (
+  return (
     <div className="w-full">
       <div className="flex w-full items-center justify-between">
         <h1 className={`${lusitana.className} text-2xl`}>Invoices</h1>
